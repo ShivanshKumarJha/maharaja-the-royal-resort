9b70af28ec0069318985bffd2516be75cb463c2f
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2024-07-03 15:13:36 +0530

    formatted the text

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,27 +1,26 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { getBookings } from "../../services/apiBookings";
-import { useSearchParams } from "react-router-dom";
-import { PAGE_SIZE } from "../../utils/constants";
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
+import { PAGE_SIZE } from '../../utils/constants';
 
 export function useBookings() {
   const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   // FILTER
-  const filterValue = searchParams.get("status");
+  const filterValue = searchParams.get('status');
   const filter =
-    !filterValue || filterValue === "all"
+    !filterValue || filterValue === 'all'
       ? null
-      : { field: "status", value: filterValue };
-  // { field: "totalPrice", value: 5000, method: "gte" };
+      : { field: 'status', value: filterValue };
 
   // SORT
-  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
-  const [field, direction] = sortByRaw.split("-");
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
   const sortBy = { field, direction };
 
   // PAGINATION
-  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+  const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
 
   // QUERY
   const {
@@ -29,7 +28,7 @@ export function useBookings() {
     data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy, page],
+    queryKey: ['bookings', filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
@@ -38,13 +37,13 @@ export function useBookings() {
 
   if (page < pageCount)
     queryClient.prefetchQuery({
-      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryKey: ['bookings', filter, sortBy, page + 1],
       queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
     });
 
   if (page > 1)
     queryClient.prefetchQuery({
-      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryKey: ['bookings', filter, sortBy, page - 1],
       queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
     });
 
