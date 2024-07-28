6097e697b633e0ff2b6405adbde0cdbed40339d4
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2024-07-28 18:33:07 +0530

    uploaded the sample data

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -3,6 +3,7 @@ import Logo from './Logo';
 import MainNav from './MainNav';
 // import Uploader from '../data/Uploader';
 
+
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
@@ -19,7 +20,7 @@ function Sidebar() {
     <StyledSidebar>
       <Logo />
       <MainNav />
-      {/* <Uploader /> */}
+       {/*<Uploader />*/}
     </StyledSidebar>
   );
 }
